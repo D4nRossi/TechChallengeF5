10a0c582c4bdf2aca282b0485cb534daec1fda8a
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2025-07-30 18:50:04 -0300

    teste ci/cd

--- pedido-service/Migrations/20250730213335_justificativa.Designer.cs
+++ pedido-service/Migrations/20250730213335_justificativa.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pedido_service.Data;
 
@@ -11,9 +12,11 @@ using pedido_service.Data;
 namespace pedido_service.Migrations
 {
     [DbContext(typeof(PedidoDbContext))]
-    partial class PedidoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250730213335_justificativa")]
+    partial class justificativa
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ pedido-service/Migrations/20250730213335_justificativa.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pedido_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class justificativa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JustificativaCancelamento",
+                table: "Pedidos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JustificativaCancelamento",
+                table: "Pedidos");
+        }
+    }
+}
